3d68735bfcc2822e3387797afd1144fbf0a74495
Dominik Hladík <dominik.hladik@seznam.cz>
2024-02-10 18:42:48 +0100

    chore(dist): dist content changed

--- scripts/prepare-dist.ts
+++ scripts/prepare-dist.ts
@@ -6,8 +6,11 @@ import distPackage from '../dist/package.json';
 // Modify package.json in dist folder
 const pkg: Record<string, any> = distPackage;
 
-delete pkg.scripts.prepare;
-delete pkg.scripts['gpr:setup'];
+pkg.scripts = {};
+pkg.devDependencies = {};
+pkg.engines = {
+  node: '>=12'
+};
 
 writeFileSync(join(__dirname, '..', 'dist', 'package.json'), JSON.stringify(pkg, null, 2));
 console.log('File package.json modified:', pkg);
